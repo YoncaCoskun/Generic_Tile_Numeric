ace1430d0395cc1a7b2f6ac3b7216e350b58bd83
kalimu123321 <yonca.coskun@hotmail.com.tr>
2017-05-11 10:45:01 +0300

    Set Interval added Page Controller.

--- Page.controller.js
+++ Page.controller.js
@@ -3,22 +3,34 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/mvc/Controller', 'sap/ui/model/
 
 	var PageController = Controller.extend("sap.m.sample.TileContainer.Page", {
 		onInit: function() {
-				var that = this;
-				var sServiceUrl = "/rim/api/rim-analytics.svc";
-				var oModel = new sap.ui.model.odata.ODataModel(sServiceUrl);
-				var oJsonModel = new sap.ui.model.json.JSONModel();
-				
+			var that = this;
+			var sServiceUrl = "/rim/api/rim-analytics.svc";
+			var oModel = new sap.ui.model.odata.ODataModel(sServiceUrl);
+			var oJsonModel = new sap.ui.model.json.JSONModel();
+
+			oModel.read("/Sensors", null, null, true, function(oData) {
+				oJsonModel.setData(oData);
+				console.log(oData.results);
+			});
+			that.getView().setModel(oJsonModel, "JModel");
+			this.getView().byId("container").setModel(this.getView().getModel("JModel"));
+
+			//Her saniyede bir model tekrar cekip tile'ların yenilenmesi
+			var jmodel = this.getView().getModel("JModel");
+			this.intervalHandle = setInterval(function() {
 				oModel.read("/Sensors", null, null, true, function(oData) {
-					oJsonModel.setData(oData);
+					jmodel.setData(oData);
 					console.log(oData.results);
 				});
-				that.getView().setModel(oJsonModel,"JModel");
-				this.getView().byId("container").setModel(this.getView().getModel("JModel"));
-				
-					
-		
-		},
+			}, 1000);
 
+		},
+		//Set interval süreklı calısmasın diye intervalin kapatılması
+		onExit: function() {
+			if (this.intervalHandle) {
+				clearInterval(this.intervalHandle);
+			}
+		},
 		handleEditPress: function(evt) {
 			var oTileContainer = this.getView().byId("container");
 			var newValue = !oTileContainer.getEditable();
